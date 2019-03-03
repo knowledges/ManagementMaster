8ad17fa909286e0b7aa8c46c3573e3168c763f00
knowledges <qbl378739311@163.com>
2019-03-03 22:31:15 +0800

    地图的添加

--- src/utils/map.js
+++ src/utils/map.js
@@ -25,3 +25,22 @@ export function rmvBAMP() {
   return localStorage.removeItem('BAMP')
 }
 
+/**
+ * 创建地图
+ * @param map map 对象
+ * @param id 渲染的id
+ * @param params x 和 y 坐标
+ * @param zom 级别
+ */
+export function createMap(map, id, params, zom) {
+  map = new window.BMap.Map(id) // 创建Map 实例
+  map.centerAndZoom(window.BMap.Point(params.lng, params.lat), zom) // 初始化地图,设置中心点坐标和地图级别
+  map.enableScrollWheelZoom()
+  map.addControl(window.BMap.NavigationControl())
+  map.addControl(window.BMap.ScaleControl())
+  map.addControl(window.BMap.OverviewMapControl())
+}
+
+export function rmvMap() {
+}
+
--- src/views/map/index.vue
+++ src/views/map/index.vue
@@ -1,25 +1,41 @@
 <!--qiu.bl-->
-<style rel="stylesheet/scss" lang="scss" scoped></style>
+<style rel="stylesheet/scss" lang="scss" scoped>
+  #maps {
+    position: absolute;
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    background: #FFF;
+  }
+</style>
 <template>
-  <div id="maps" class="container-fluid">map index.vue</div>
+  <div id="maps">
+    <!-- Map Dom -->
+  </div>
 </template>
 <script>
-import { mapGetters } from 'vuex'
+import BaiduMap from 'BaiduMap'
+// import MarkerClusterer from 'BaiduMap.markerclusterer'
 export default {
   name: 'Map',
   data() {
     return {}
   },
   computed: {
-    ...mapGetters(['BMAP'])
   },
   created() {},
   mounted() {
-    this.$store.dispatch('MP')
-      .then(respone => {
-        console.log('1234')
-        console.log(respone)
-      })
+    console.log(BaiduMap)
+//    console.log (MarkerClusterer)
+    var map = new BaiduMap.Map('maps') // 创建地图实例
+    var point = new BaiduMap.Point(118.779102, 31.981199) // 创建中心点坐标
+    map.centerAndZoom(point, 16) // 初始化地图，设置中心点坐标和地图级别
+
+    var marker = new BaiduMap.Marker(point) // 创建标注
+    map.addOverlay(marker) // 将标注添加到地图中
+
+    map.enableScrollWheelZoom() //启用滚轮放大缩小
+    map.addControl(new BaiduMap.ScaleControl()) // 比例尺
   },
   methods: {}
 }
